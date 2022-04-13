70e89f3dcd6146b3b2b8a1c43fea8da8d9772a67
liaolklk <47046450+liaolklk@users.noreply.github.com>
2022-04-13 18:13:36 +0800

    Add files via upload

---
+++ jenkinsdemo/src/main/java/com/brian/jenkinsdemo/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.brian.jenkinsdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/{page}")
+    public String page(@PathVariable String page) {
+        return page;
+    }
+}
---
+++ jenkinsdemo/src/test/java/com/brian/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
